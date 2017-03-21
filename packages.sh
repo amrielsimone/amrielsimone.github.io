016badc8b4ac3481342ebb79c9967993a47b7373
Amriel Simone <amriel@amriel.com>
2017-03-21 10:41:19 -0700

    ...

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/aroma debs
-dpkg-deb -bZgzip projects/aromatwo debs
+dpkg-deb -bZgzip projects/aroma2 debs
 dpkg-deb -bZgzip projects/lgpack debs
 dpkg-deb -bZgzip projects/yalu debs
 dpkg-deb -bZgzip projects/noduck debs
