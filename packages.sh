168f7f69837eda65a7ac495ee92e3617c73cf843
BuildTools <unconfigured@null.spigotmc.org>
2017-03-22 20:13:15 -0700

    ...

--- packages.sh
+++ packages.sh
@@ -4,4 +4,4 @@ dpkg-deb -bZgzip projects/aroma2 debs
 dpkg-deb -bZgzip projects/lgpack debs
 dpkg-deb -bZgzip projects/yalu debs
 dpkg-deb -bZgzip projects/noduck debs
-dpkg-deb -bZgzip projects/glasklart debs
+dpkg-deb -bZgzip projects/oneplus2 debs
